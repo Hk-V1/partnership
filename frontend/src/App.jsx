f0d37be962201919af3bba236ddd9277dd3292ea
Harleen Kaur <hk22.work@gmail.com>
2025-08-07 19:06:08 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MatchProjectsForm from "./components/MatchProjectsForm";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", padding: 20 }}>
+      <h1>Partnership Collaboration Platform</h1>
+      <MatchProjectsForm />
+    </div>
+  );
+}
+
+export default App;
